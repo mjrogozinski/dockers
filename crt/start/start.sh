5b0c215d53a25dff8507e57f617b6cb566297787
mjrogozinski <mjrogozinski@gmail.com>
2019-04-02 19:28:59 +0200

    Starting qt in background to have a background shell after running make run

--- crt/start/start.sh
+++ crt/start/start.sh
@@ -3,4 +3,5 @@
 cd $WORK
 git clone --branch work $REPOS/webee
 cd webee; mkdir build; cd build; cmake .. -DCMAKE_BUILD_TYPE=Debug
-/var/fpwork/tools/qtcreator/Tools/QtCreator/bin/qtcreator ../CMakeLists.txt
+/var/fpwork/tools/qtcreator/Tools/QtCreator/bin/qtcreator ../CMakeLists.txt &
+/bin/bash
