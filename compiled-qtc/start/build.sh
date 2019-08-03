6ddd8f9ec857cb66473c809bbb7b022675457649
mjrogozinski <mjrogozinski@gmail.com>
2019-08-03 23:01:57 +0200

    QtCreator now required llvm 8. Updating packager so ClangCodeModel et al are available

--- compiled-qtc/start/build.sh
+++ compiled-qtc/start/build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p $WORK/qt
 cd $WORK/qt
+export PATH="${PATH}:/usr/lib/llvm-8/bin"
 git clone -b 'v5.13.0' --single-branch --depth 1 --recurse-submodules -j40 https://github.com/qt/qt5.git
 cd qt5
 ./configure -prefix $PWD/qtbase -opensource -confirm-license -nomake tests
