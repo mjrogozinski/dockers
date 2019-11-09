39aaab50f345f737b922161f4496cb92c668eba1
mjrogozinski <mjrogozinski@gmail.com>
2019-11-09 15:35:15 +0100

    Adding quiet flag to the make for building qt and qtcreator

--- compiled-qtc/start/build.sh
+++ compiled-qtc/start/build.sh
@@ -6,11 +6,11 @@ export PATH="${PATH}:/usr/lib/llvm-8/bin"
 git clone -b 'v5.13.0' --single-branch --depth 1 --recurse-submodules -j40 https://github.com/qt/qt5.git
 cd qt5
 ./configure -prefix $PWD/qtbase -opensource -confirm-license -nomake tests
-make
+make --quiet
 
 cd $WORK/qt
 git clone --single-branch --depth 1 https://github.com/qt-creator/qt-creator.git
 
 cd qt-creator
 $WORK/qt/qt5/qtbase/qmake/qmake -r
-make
+make --quiet
