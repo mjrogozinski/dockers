5f6dcc92f7830fe4cc0c9ef7387aafbca31b1657
mjrogozinski <mjrogozinski@gmail.com>
2019-12-23 00:02:58 +0100

    Updating qt version

--- compiled-qtc/start/build.sh
+++ compiled-qtc/start/build.sh
@@ -3,7 +3,7 @@
 mkdir -p $WORK/qt
 cd $WORK/qt
 export PATH="${PATH}:/usr/lib/llvm-8/bin"
-git clone -b 'v5.13.0' --single-branch --depth 1 --recurse-submodules -j40 https://github.com/qt/qt5.git
+git clone -b 'v5.14.0' --single-branch --depth 1 --recurse-submodules -j40 https://github.com/qt/qt5.git
 cd qt5
 ./configure -prefix $PWD/qtbase -opensource -confirm-license -nomake tests
 make --quiet
--- compiled-qtc/start/start.sh
+++ compiled-qtc/start/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p $WORK/demoronizer/build
 cd $WORK/demoronizer/build
+cmake ..
 $WORK/qt/qt-creator/bin/qtcreator .. &
 /bin/bash
