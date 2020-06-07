de9a07175f0a7e11b939ad70effaefe353ac1320
mjrogozinski <mjrogozinski@gmail.com>
2020-06-07 09:11:19 +0200

    Updating qtc and cmake versions

--- crt/start/start.sh
+++ crt/start/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $WORK/cryptator
-/var/fpwork/tools/qtcreator/Tools/QtCreator/bin/qtcreator CMakeLists.txt &
+/var/fpwork/tools/qtcreator/Tools/QtCreator/bin/qtcreator . &
 /bin/bash
