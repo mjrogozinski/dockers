1a3de1d312c4e9548371507b08e195ad8f91534b
mjrogozinski <mjrogozinski@gmail.com>
2020-09-19 15:34:52 +0200

    Updating branches of qt and qtc

--- compiled-qtc/start/build.sh
+++ compiled-qtc/start/build.sh
@@ -3,14 +3,16 @@
 mkdir -p $WORK/qt
 cd $WORK/qt
 export PATH="${PATH}:/usr/lib/llvm-8/bin"
-git clone -b 'v5.14.0' --single-branch --depth 1 --recurse-submodules -j40 https://github.com/qt/qt5.git
+git clone -b 'v5.15.1' --single-branch --depth 1 --recurse-submodules -j40 git@github.com:qt/qt5.git
 cd qt5
+sudo update-alternatives --config gcc-6
 ./configure -prefix $PWD/qtbase -opensource -confirm-license -nomake tests
 make --quiet
 
 cd $WORK/qt
-git clone --single-branch --depth 1 https://github.com/qt-creator/qt-creator.git
+git clone -b 'v4.13.1' --single-branch --depth 1 git@github.com:qt-creator/qt-creator.git
 
 cd qt-creator
 $WORK/qt/qt5/qtbase/qmake/qmake -r
 make --quiet
+sudo update-alternatives --config gcc-9
